541dfc96b3a1a53513af2cd63e19c1c6fa9be80c
Daniel Pang <hdpang@uwaterloo.ca>
2017-10-08 00:21:06 -0400

    First commit

---
+++ a_search.py
@@ -0,0 +1,192 @@
+import sys
+from scipy.spatial import distance
+from queue import PriorityQueue
+import copy
+
+# Class for the city nodes
+# Used to stores cities as nodes in a graph
+# Has name of city along with x and y positions on a 2D plane
+class Node:
+	def __init__(self, city="", x=0, y=0):
+		self.name = city
+		self.x = x
+		self.y = y
+
+# Class for the states
+
+class State:
+	def __init__(self, u_nodes=[], v_nodes=[], current_node=None, f_score=sys.maxsize):
+		self.unvisited_nodes = u_nodes
+		self.visited_nodes = v_nodes
+		self.node = current_node
+		self.f_score = f_score
+		self.leaf = True
+
+# Method to find the node in Q with the lowest weight
+def min_key(nodes, key, mst_set):
+	minimum = sys.maxsize
+	for node in nodes:
+		if key[node.name] < minimum and mst_set[node.name] == False:
+			minimum = key[node.name]
+			min_index = node
+	return min_index
+
+# Prim's algorithm to find the minimum spanning tree of all unvisited nodes
+def prim(unvisited_nodes, edges):
+	nodes = copy.deepcopy(unvisited_nodes)
+	key = {}
+	pred = {}
+	mst_set = {}
+	# Initialize all dict
+	for n in nodes:
+		pred[n.name] = None
+		key[n.name] = sys.maxsize
+		mst_set[n.name] = False
+	key[nodes[0].name] = 0
+	pred[nodes[0].name] = None
+
+	for n in nodes:
+		# Find the node with the smallest weight
+		u = min_key(nodes, key, mst_set)
+		mst_set[u.name] = True
+		for node in nodes:
+			if node.name != u and mst_set[node.name] == False and key[node.name] > edges[node.name, u.name]:
+				key[node.name] = edges[node.name,u.name]
+				pred[node.name] = u
+	mst_cost = 0
+	for key in pred:
+		if pred[key] != None:
+			mst_cost += edges[key, pred[key].name]
+	return mst_cost
+
+# Heuristic function
+# h(n) at node n is the minimum spanning tree of remaining cities +
+# distance to the NEAREST unvisited node + nearest distance from unvisted node
+# to the starting node
+def h(cur_node, unvisited_nodes, edges):
+	# If we are at the goal state (visited all nodes)
+	# then h(n) = 0
+	if len(unvisited_nodes) == 0:
+		return 0
+	# Calculate distance to the NEAREST unvisited node from current node
+	nearest_unvisited = sys.maxsize
+	for n in unvisited_nodes:
+		if n.name != cur_node.name and edges[n.name, cur_node.name] < nearest_unvisited:
+			nearest_unvisited = edges[n.name, cur_node.name]
+	# Calculate NEAREST distance from an unvisited node to the start node
+	nearest_unvisited_to_start = sys.maxsize
+	for n in unvisited_nodes:
+		if edges['A', n.name] < nearest_unvisited_to_start:
+			nearest_unvisited_to_start = edges['A', n.name]
+	# Calculate nearest distance to travel all other unvisited nodes
+	return prim(unvisited_nodes, edges) + nearest_unvisited + nearest_unvisited_to_start
+
+# Similar to g(n), cost of the path from start node to n
+# cur_path: is the list of nodes visited in order
+def g(cur_path, edges):
+	cost = 0
+	for i in range(0, len(cur_path) - 1):
+		cost += edges[cur_path[i].name, cur_path[i+1].name]
+	return cost
+
+# Determines the f score of the current state
+# f(n) = g(n) + h(n)
+def f(cur_path, cur_node, unvisited_nodes, edges):
+	return g(cur_path, edges) + h(cur_node, unvisited_nodes, edges)
+
+# Determines the cost of the chosen path
+def build_path(path, edges):
+	cost = 0
+	lst = []
+	freq = {}
+	for i in range(0, len(path) - 1):
+		lst.append(path[i].name)
+		cost += edges[path[i].name, path[i+1].name]
+	# Add cost from last city to start city
+	lst.append('A')
+	cost += edges[path[len(path) - 1].name, path[0].name]
+	return (lst,cost)
+
+# Remove node from the list of nodes
+# Wrote my own function because I wasn't sure what Python was doing
+def remove_node(unvisited_nodes, city):
+	new_lst = copy.deepcopy(unvisited_nodes)
+	# print("Remove", city)
+	for i in range(0, len(new_lst)):
+		if new_lst[i].name == city:
+			new_lst.pop(i)
+			break
+	return new_lst
+
+# Implements the main A* search algorithm
+def a_search(unvisited_nodes, edges):
+	cur_path = []
+	# Create initial start state with the starting city A
+	start_node = unvisited_nodes[0]
+	unvisited_nodes = remove_node(unvisited_nodes, 'A')
+	f_start = f(cur_path, start_node, unvisited_nodes, edges)
+	start_state = State(unvisited_nodes, [start_node], start_node, f_start)
+	num_states = 1
+	open_set = PriorityQueue()
+	open_set.put((f_start, 1, start_state))
+	# Continue expanding nodes until there are no more nodes to expand
+	while not open_set.empty():
+		# Obtain state with smallest f_score, top node from min heap
+		item = open_set.get()
+		cur_state = item[2]
+		# We have chosen cur_state leaf node for expansions
+		cur_path.append(cur_state.node)
+		# If we have no more nodes to visited, we have reached goal state
+		if len(cur_state.unvisited_nodes) == 0:
+			num_states += 1 # for the goal state
+			print("Number of states generated", num_states)
+			return num_states
+		# Expand the nodes, adding the next level of nodes
+		for unvisited in cur_state.unvisited_nodes:
+			num_states += 1
+			visited = copy.deepcopy(cur_state.visited_nodes)
+			visited.append(unvisited)
+			temp_lst = remove_node(cur_state.unvisited_nodes, unvisited.name)
+			#print(len(temp_lst))
+			# Calculate f_score for this new state
+			f_score = f(visited, unvisited, temp_lst, edges)
+			s = State(temp_lst, visited, unvisited, f_score)
+			# Add new state to the open_set of states to explore
+			open_set.put((f_score, num_states, s))
+	return 0
+
+# Creates a node for each city
+def create_nodes(file):
+	next(file)
+	lst = []
+	for line in file:
+		line = line.rstrip('\n')
+		vals = line.split()
+		#print(vals)
+		n = Node(str(vals[0]), int(vals[1]), int(vals[2]))
+		lst.append(n)
+	#print("Finished creating nodes")
+	return lst
+
+# Calculates the cost (Euclidean distance) of travel
+# between 2 nodes
+def calculate_cost(lst):
+	dict = {}
+	for node in lst:
+		for other_node in lst:
+			a = (node.x,node.y)
+			b = (other_node.x,other_node.y)
+			dst = distance.euclidean(a,b)
+			dict[node.name,other_node.name] = dst
+	#print("Finished calculating distances")
+	return dict
+
+def main():
+	# Open file
+	file = open(sys.argv[1], 'r')
+	unvisited_nodes = create_nodes(file)
+	edge_costs = calculate_cost(unvisited_nodes)
+	print(a_search(unvisited_nodes, edge_costs))
+
+if __name__ == '__main__':
+	main()
